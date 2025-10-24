e05f457d1afc89bc996b6ae8651d8c410ee01743
Franco Fabris <franco.fabris@est.fi.uncoma.edu.ar>
2025-10-24 10:33:31 -0300

    Actualizacion

--- TP2 - Web Estatica/logic.js
+++ TP2 - Web Estatica/logic.js
@@ -1,53 +1,88 @@
 const recetaHtml = (id) => {
-    const receta = fetch();
+    const recetasJson = awaitfetch(recipes.json);
+    const recetas = await (recetasJson => recetasJson.json());
+    const receta = recetas.find((receta) => receta.id === id);
+    let ingredientes;
+    let preparacion;
+
+    receta.ingredientes.forEach((ingrediente) => {
+        ingredientes += `<li>${ingrediente}</li>`;
+    });
+    receta.preparacion.forEach((paso) => {
+        preparacion += `<li>${paso}</li>`;
+    });
     const html = `    
-    <article class="header-receta">
-        <section class="categoria-receta"></section>
-
-        <section class="titulo-receta">
-          <h1>Pesto alla Genovese</h1>
-        </section>
-      </article>
-
-      <article class="main-receta">
-        <section class="foto-receta">
-          <img
-            src="Images/pesto.jpg"
-            alt="Pappardelle con Salchicha"
-            class="imagen-principal"
-          />
-        </section>
-
-        <section class="descripcion-receta">
-          <p>
-            <strong
-              >El Pesto alla Genovese es un clásico de la región de Liguria,
-              Italia, famoso por su frescura y aroma gracias a la albahaca y el
-              aceite de oliva extra virgen.</strong
-            >
-          </p>
-        </section>
-
-        <section class="grid-items">
-          <article class="grid-item">
-            <h4>⏱️ Tiempo de Preparación</h4>
-            <p>30 minutos</p>
+        <article class="header-receta">
+            <section class="categoria-receta"></section>
+    
+            <section class="titulo-receta">
+              <h1>${receta.nombre}</h1>
+            </section>
           </article>
+    
+          <article class="main-receta">
+            <section class="foto-receta">
+              <img
+                src="${receta.imagen-principal}"
+                alt="${receta.alt}"
+                class="imagen-principal"
+              />
+            </section>
+    
+            <section class="descripcion-receta">
+              <p>
+                <strong
+                  >${receta.descripcion}</strong
+                >
+              </p>
+            </section>
+    
+            <section class="grid-items">
+              <article class="grid-item">
+                <h4>⏱️ Tiempo de Preparación</h4>
+                <p>${receta.categoria.tiempo}</p>
+              </article>
+    
+              <article class="grid-item">
+                <h4> Dificultad</h4>
+                <p>${receta.categoria.dificultad}</p>
+              </article>
+    
+              <article class="grid-item">
+                <h4> Porciones</h4>
+                <p>${receta.categoria.porciones}</p>
+              </article>
+    
+            </section>"
 
-          <article class="grid-item">
-            <h4> Dificultad</h4>
-            <p>${receta.categoria.dificultad}</p>
-          </article>
+        <article class ="cuerpo-receta">
+            <div class="resumen-receta">
+                <section class="seccion-receta-2">
 
-          <article class="grid-item">
-            <h4> Porciones</h4>
-            <p>4 personas</p>
-          </article>
+                    <h2>Ingredientes</h2>
+                    <ul>
+                        ${ingredientes}
+                    </ul>
+                </section>
 
-          <article class="grid-item">
-            <h4> Categoría</h4>
-            <p>Plato Principal</p>
-          </article>
-        </section>"
-    `
+                <section class="seccion-receta-2">
+                    <h2>Preparación</h2>
+                    <ul>
+                        ${preparacion}
+                    </ul>
+
+                </section>
+            </div>
+
+            <section class="seccion-receta-2">
+                <h2>Descripción Detallada de la Preparación</h2>
+                <p>
+                    ${receta.detallado}
+                </p>
+            </section>
+        </article>
+
+    </article>
+    `;
+    appendChild(html, document.querySelector('main'));
 }
