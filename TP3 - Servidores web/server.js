92837af09e59fae933f9c060382f978c644c633d
Casc0 <fabern2110@gmail.com>
2025-11-20 20:04:37 -0300

    cambios 2

--- TP3 - Servidores web/server.js
+++ TP3 - Servidores web/server.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(tp2Path, 'img')));
 
 // RUTA : /cocinaitaliana
 // Alguien visita http://localhost:4000/cocinaitaliana
-app.get('/cocinaitaliana', (req, res) => {
+app.get('/cocinaItaliana', (req, res) => {
     // Redirige al archivo principal de la página
     res.redirect('/index.html'); 
 });
@@ -32,9 +32,18 @@ app.get('/cocinaitaliana', (req, res) => {
 // RUTA RAÍZ: /
 app.get('/', (req, res) => {
     // Lo redirigimos a la nueva URL 
-    res.redirect('/cocinaitaliana');
+    res.redirect('/cocinaItaliana');
 });
 
+app.get('/cocinaItaliana/recetas', async (req, res) =>{
+    const list = await fetch("./recipes.json");
+    console.log(list.json)
+})
+
+app.get('/cocinaItaliana/recetas/${id}', async (req, res) =>{
+    const list = await fetch("./recipes.json");
+    console.log(list.json)
+})
 
 // =======================================================
 // INICIO DEL SERVIDOR
@@ -43,6 +52,6 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     // Mensajes de confirmación
     console.log(`Servidor Express listo para el HTML.`);
-    console.log(`\nURL Principal: http://localhost:${PORT}/cocinaitaliana`);
+    console.log(`\nURL Principal: http://localhost:${PORT}/cocinaItaliana`);
     console.log(`Presionar Ctrl + C para detener el servidor.`);
 });
