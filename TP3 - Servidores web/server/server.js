815b373395d3c0240fc64728306ac9d268336212
Franco Fabris <80720027+Casc0@users.noreply.github.com>
2026-01-15 01:46:57 -0300

    a

--- TP3 - Servidores web/server/server.js
+++ TP3 - Servidores web/server/server.js
@@ -1,9 +1,14 @@
 const app = require("./app"); // Importa la app de Express desde index.js
 const PORT = 4000; // Define el puerto en el que se ejecutará la app
 
-app.listen(PORT, () => {
-  // Mensajes de confirmación
-  console.log(`Servidor Express listo para el HTML.`);
-  console.log(`\nURL Principal: http://localhost:${PORT}/cocinaItaliana`);
-  console.log(`Presionar Ctrl + C para detener el servidor.`);
-});
+// Para desarrollo local
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Servidor Express listo para el HTML.`);
+    console.log(`\nURL Principal: http://localhost:${PORT}/cocinaItaliana`);
+    console.log(`Presionar Ctrl + C para detener el servidor.`);
+  });
+}
+
+// Exportar para Vercel Serverless Functions
+module.exports = app;
